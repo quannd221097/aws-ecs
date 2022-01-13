5c20b8ea6a87c9ab3c01417be2f96948ba626290
quan2210quan <quan2210quan@gmail.com>
2022-01-13 22:00:51 +0700

    update

--- mysql.tf
+++ mysql.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "mysql" {
     instance_class            = "db.t2.micro"
     name                      = "worker_db"
     username                  = "worker"
-    password                  = "worker"
+    password                  = "worker123456"
     port                      = "3306"
     db_subnet_group_name      = aws_db_subnet_group.db_subnet_group.id
     vpc_security_group_ids    = [aws_security_group.rds_sg.id, aws_security_group.ecs_sg.id]
